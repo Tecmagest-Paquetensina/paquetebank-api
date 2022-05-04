f59384f33ae484650c57702bb8890562e3c20c5f
Francelino Paquete <tecmagest@gmail.com>
2022-05-03 21:59:23 -0300

    Inicializar o repositorio do Projeto PaqueteBank API Rest

---
+++ src/main/java/com/paquetebank/paquetebank/api/PaquetebankApiApplication.java
@@ -0,0 +1,14 @@
+package com.paquetebank.paquetebank.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaquetebankApiApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(PaquetebankApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/paquetebank/paquetebank/api/PaquetebankApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paquetebank.paquetebank.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaquetebankApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
